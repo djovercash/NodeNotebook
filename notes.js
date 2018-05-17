43bb3102ed20fec96a72d4db9eb4053ec4580c90
github username <github email address>
2018-05-17 15:26:32 -0400

    Adding Note to File

--- notes.js
+++ notes.js
@@ -1,7 +1,27 @@
 console.log("starting notes.js")
+const fs = require('fs')
+
 
 var addNote = (title, body) => {
-  console.log("Adding note:", title, body)
+  let notes = []
+  let note = {
+    title,
+    body
+  };
+
+  try {
+    notes = JSON.parse(fs.readFileSync('notes-data.json'))
+  } catch (e) {
+
+  }
+
+  var duplicateNotes = notes.filter((note) => note.title === title)
+  if (duplicateNotes.length === 0){
+    notes.push(note)
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes))
+  } else {
+    console.log("Duplicate Title. Please choose a new title")
+  }
 };
 
 var getAll = () => {
--- playground/json.js
+++ playground/json.js
@@ -1,7 +1,30 @@
-var obj = {
-  name: "Drew"
-};
+// OBJECT TO STRING
+// var obj = {
+//   name: "Drew",
+//   age: 30
+// };
+//
+// var stringObj = JSON.stringify(obj)
+// console.log(typeof stringObj)
+// console.log(stringObj)
 
-var stringObj = JSON.stringify(obj)
-console.log(typeof stringObj)
-console.log(stringObj)
+// STRING TO OBJECT
+// var personString = '{"name": "Drew", "age": 30}';
+// var personObj = JSON.parse(personString)
+// console.log(typeof personObj)
+// console.log(personObj)
+
+const fs = require('fs');
+
+var originalNote = {
+  title: "Yas Queen",
+  body: "My body be full of Yas QUEENS"
+}
+var originalNoteStr = JSON.stringify(originalNote)
+
+fs.writeFileSync('notes.json', originalNoteStr)
+
+var noteString = fs.readFileSync('notes.json')
+var note = JSON.parse(noteString)
+console.log(typeof note)
+console.log(note.title)
