9e5d92ce5d7c67364f8778d9c581f0f8805e89ea
github username <github email address>
2018-05-17 14:26:01 -0400

    Cleaned up examples

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 console.log("starting app.js")
 
+const fs = require('fs');
+const _ = require('lodash');
+
+const notes = require('./notes.js')
+
+
+
+
+
+
+
+
+
 
 
 // This is seeing if there is a file. If not, it makes it. If it does, it adds to it
-const fs = require('fs');
-
-// This is returning the information about the logged in user
-const os = require('os')
-// var user = os.userInfo()
-
-// This is requiring a file we created
-const notes = require('./notes.js')
+// const fs = require('fs');
+//
+// // This is returning the information about the logged in user
+// const os = require('os')
+// // var user = os.userInfo()
+//
+// // This is requiring a file we created
+// const notes = require('./notes.js')
 
 // fs.appendFile('greetingsUser.txt', `Hello, ${user.username}! You are ${notes.age}`, function(err) {
 //   if (err) {
@@ -25,4 +38,11 @@ const notes = require('./notes.js')
 // var addResults = notes.add(2, 3);
 // console.log(addResults)
 
-const _ = require('lodash')
+
+// Check if string & return only unique values
+// const _ = require('lodash');
+// console.log(_.isString(true));
+// console.log(_.isString("Drew"));
+//
+// console.log(_.uniq("asdfasd"));
+// console.log(_.uniq([2, 4, 8, 2]));
--- notes.js
+++ notes.js
@@ -1,13 +1,19 @@
 console.log("starting notes.js")
 
 
-// Sending a function from notes.js to app.js
-module.exports.addNote = () => {
-  console.log("addNote")
-  return 'New Note'
-}
 
-module.exports.add = (a, b) => {
-  let result = a + b
-  return result
-}
+
+
+
+
+
+// Sending a function from notes.js to app.js
+// module.exports.addNote = () => {
+// //   console.log("addNote")
+//   return 'New Note'
+// }
+//
+// module.exports.add = (a, b) => {
+//   let result = a + b
+//   return result
+// }
