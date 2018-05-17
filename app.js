31c35785e4c8dbd8f983d2a646885e5f7a6deabf
github username <github email address>
2018-05-17 13:15:22 -0400

    Using OS and FS for require in Node

--- app.js
+++ app.js
@@ -4,7 +4,18 @@ console.log("Starting App")
 
 // This is seeing if there is a file. If not, it makes it. If it does, it adds to it
 const fs = require('fs');
-fs.appendFile('greetings.txt', 'Hello World', function(err) {
+// fs.appendFile('greetings.txt', 'Hello World', function(err) {
+//   if (err) {
+//     console.log('Unable to write to file')
+//   }
+// })
+
+// This is returning the information about the logged in user
+const os = require('os')
+var user = os.userInfo()
+
+
+fs.appendFile('greetingsUser.txt', `Hello, ${user.username}`, function(err) {
   if (err) {
     console.log('Unable to write to file')
   }
