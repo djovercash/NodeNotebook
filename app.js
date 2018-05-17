64ce159ea2181a8aac793d8117795da8437d3508
github username <github email address>
2018-05-17 14:00:18 -0400

    Add a function in notes.js and exported it to app.js

--- app.js
+++ app.js
@@ -4,21 +4,20 @@ console.log("starting app.js")
 
 // This is seeing if there is a file. If not, it makes it. If it does, it adds to it
 const fs = require('fs');
-// fs.appendFile('greetings.txt', 'Hello World', function(err) {
+
+// This is returning the information about the logged in user
+const os = require('os')
+// var user = os.userInfo()
+
+// This is requiring a file we created
+const notes = require('./notes.js')
+
+// fs.appendFile('greetingsUser.txt', `Hello, ${user.username}! You are ${notes.age}`, function(err) {
 //   if (err) {
 //     console.log('Unable to write to file')
 //   }
 // })
 
-// This is returning the information about the logged in user
-const os = require('os')
-var user = os.userInfo()
 
-
-fs.appendFile('greetingsUser.txt', `Hello, ${user.username}`, function(err) {
-  if (err) {
-    console.log('Unable to write to file')
-  }
-})
-
-const notes = require('./notes.js')
+var results = notes.addNote();
+console.log(results);
--- notes.js
+++ notes.js
@@ -1,1 +1,8 @@
 console.log("starting notes.js")
+
+
+// Sending a function from notes.js to app.js
+module.exports.addNote = () => {
+  console.log("addNote")
+  return 'New Note'
+}
