89ac6f13ad7876298527bac6323a9a2694951c58
github username <github email address>
2018-05-17 14:34:21 -0400

    Create commands for user from terminal

--- notes.js
+++ notes.js
@@ -7,6 +7,40 @@ console.log("starting notes.js")
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Sending a function from notes.js to app.js
 // module.exports.addNote = () => {
 // //   console.log("addNote")
