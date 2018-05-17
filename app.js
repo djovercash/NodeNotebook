410b3f324cf826c22a06fcfa24b810b2fc89431d
github username <github email address>
2018-05-17 13:11:31 -0400

    Understanding Require

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 console.log("Starting App")
 
+
+
+// This is seeing if there is a file. If not, it makes it. If it does, it adds to it
 const fs = require('fs');
+fs.appendFile('greetings.txt', 'Hello World', function(err) {
+  if (err) {
+    console.log('Unable to write to file')
+  }
+})
